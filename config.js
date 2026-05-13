583057ef42c7a196cebbc8e06655bba8fda9fc74
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-05-13 16:02:59 +0900

    Add configuration file for API and sheet ID

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  SHEET_ID: '1V14F3Fv8_XmlKKqPvWPMV18Ko0CMTw9S65bdvgBO3mk',
+  API_URL: 'https://script.google.com/macros/s/AKfycby7yh2kqNXhjSig8La9tPEnGvDEv-KTWIn0AkyC27og54O0kHspf_7xEOJEbimKJtRFIg/exec'
+};
